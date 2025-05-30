ef575ba971ac49020916ebe4d86cd0ceac290d98
Ivan-Romo <ivanromontil@gmail.com>
2025-05-30 09:05:25 +0200

    bug solving

--- ComputeShaderProject/Assets/Project/PixelSimulatorGPU.cs
+++ ComputeShaderProject/Assets/Project/PixelSimulatorGPU.cs
@@ -1,3 +1,4 @@
+// === PixelSimulatorGPU.cs ===
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,22 +22,11 @@ public class PixelSimulatorGPU : MonoBehaviour
 
     void Start()
     {
-
         texture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGBFloat);
         texture.enableRandomWrite = true;
         texture.Create();
         display.texture = texture;
 
-        //
-
-        //agentPositions = new Vector2[agentCount];
-        //agentDirections = new Vector2[agentCount];
-        //for (int i = 0; i < agentCount; i++)
-        //{
-        //    agentPositions[i] = new Vector2(Random.Range(0, width), Random.Range(0, height));
-        //    float angle = Random.Range(0, Mathf.PI * 2);
-        //    agentDirections[i] = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        //}
         Vector2 center = new Vector2(width / 2f, height / 2f);
         float spawnRadius = Mathf.Min(width, height) * 0.5f;
 
@@ -54,8 +44,7 @@ public class PixelSimulatorGPU : MonoBehaviour
             Vector2 toCenter = (center - pos).normalized;
             agentDirections[i] = toCenter;
         }
-        
-        //
+
         agentPositionsBuffer = new ComputeBuffer(agentCount, sizeof(float) * 2);
         agentDirectionsBuffer = new ComputeBuffer(agentCount, sizeof(float) * 2);
         agentPositionsBuffer.SetData(agentPositions);
@@ -76,14 +65,18 @@ public class PixelSimulatorGPU : MonoBehaviour
         computeShader.SetFloat("sensorDistance", sensorDistance);
         computeShader.SetFloat("turnSpeed", turnSpeed);
 
-
         int fadeKernel = computeShader.FindKernel("FadeTrail");
-        computeShader.SetFloat("fadeFactor", 0.99f); 
+        computeShader.SetFloat("fadeFactor", 0.99f);
         computeShader.SetTexture(fadeKernel, "Result", texture);
         computeShader.SetInt("width", width);
         computeShader.SetInt("height", height);
         computeShader.Dispatch(fadeKernel, Mathf.CeilToInt(width / 8f), Mathf.CeilToInt(height / 8f), 1);
 
+        int smoothKernel = computeShader.FindKernel("SmoothTrail");
+        computeShader.SetTexture(smoothKernel, "Result", texture);
+        computeShader.SetInt("width", width);
+        computeShader.SetInt("height", height);
+        computeShader.Dispatch(smoothKernel, Mathf.CeilToInt(width / 8f), Mathf.CeilToInt(height / 8f), 1);
 
         computeShader.SetInt("agentCount", agentCount);
         computeShader.Dispatch(kernel, Mathf.CeilToInt(agentCount / 256f), 1, 1);
